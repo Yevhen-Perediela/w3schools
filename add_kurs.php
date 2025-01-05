e367f508e86635bf76bbe4a31fe0f8f0226b4cb3
Yevhen Perediela <peredelaevgen23@gmail.com>
2025-01-05 15:15:45 +0100

    add elment after smth, add-button

--- add_kurs.php
+++ add_kurs.php
@@ -8,13 +8,13 @@
     <style>
         .image-container {
             display: flex; /* Umożliwia wyświetlanie obrazów obok siebie */
-            justify-content: center; /* Wyśrodkowanie obrazów */
-            margin: 10px 0; /* Margines dla obrazów */
+            justify-content: center; 
+            margin: 10px 0;
         }
         .image-container img {
             max-width: 45%; /* Ustaw maksymalną szerokość obrazów */
-            height: auto; /* Ustaw automatyczną wysokość */
-            margin: 0 5px; /* Margines między obrazami */
+            height: auto;
+            margin: 0 5px; 
         }
         .header-input {
             font-size: 24px; /* Domyślny rozmiar czcionki dla h2 */
@@ -26,18 +26,18 @@
             font-size: 32px; /* Rozmiar czcionki dla h1 */
         }
         .add-button {
-            display: none; /* Ukryj przycisk */
-            position: absolute;
-            left: -50px; /* Pozycjonowanie przycisku */
-            background-color: #3a3f47; /* Kolor tła przycisku */
-            color: white; /* Kolor tekstu przycisku */
-            border: none; /* Bez obramowania */
-            padding: 5px; /* Padding dla przycisku */
-            cursor: pointer; /* Kursor wskazujący */
+            /* display: none; Ukryj przycisk */
+            /* position: absolute; */
+            /* left: -50px; Pozycjonowanie przycisku */
+            /* background-color: #3a3f47; Kolor tła przycisku */
+            /* color: white; Kolor tekstu przycisku */
+            /* border: none; Bez obramowania */
+            /* padding: 5px; 
+            cursor: pointer; 
         }
         .element-container {
             position: relative; /* Umożliwia pozycjonowanie przycisku w odniesieniu do elementu */
-        }
+        } */
     </style>
 </head>
 <body>
@@ -49,13 +49,14 @@
         <div id='element'>
             <textarea id="auto-resize-textarea" rows='1' placeholder='Wpisz text..' oninput="this.style.height = ''; this.style.height = this.scrollHeight + 'px';"></textarea>
         </div>
-        <button id="main-control">+ Dodaj</button>
+        <button id="main-control" onclick='AddElementMainBtn()'>+ Dodaj</button>
         <div id="menu">
-            <div onclick="addElement('h2')">Dodaj H2</div>
-            <div onclick="addElement('h3')">Dodaj H3</div>
-            <div onclick="addElement('textarea')">Dodaj Textarea</div>
-            <div onclick="addImage()">Dodaj Zdjęcie</div>
+            <div data-type="h2">Dodaj H2</div>
+            <div data-type="h3">Dodaj H3</div>
+            <div data-type="textarea">Dodaj Textarea</div>
+            <div data-type="image">Dodaj Zdjęcie</div>
         </div>
+
     </div>
     <script src='js/add_kurs.js'></script>
 </body>
