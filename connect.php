d07b68f2e40fd685237427ac610aaf41904bb4f8
Yevhen Perediela <93733182+Yevhen-Perediela@users.noreply.github.com>
2025-01-08 14:56:08 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
     $host = 'localhost';
-    $dbname = 'w3schools';
-    $username = 'root';
-    $password = '';
+    $dbname = 'u719381222_w3schools';
+    $username = 'u719381222_root';
+    $password = '123Qwerty321!';
     $conn = new mysqli($host, $username, $password, $dbname);    
 ?>
